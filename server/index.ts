5e759d81231bcbf5a113bfed147259f5f11eeed8
Dominik Sakic <dominik.sakic@yahoo.com>
2024-07-10 18:27:26 +0900

    feat: add middleware to handle errors

--- server/index.ts
+++ server/index.ts
@@ -1,13 +1,20 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import errorHandler from "./middleware";
 
 dotenv.config();
 
 const app: Express = express();
 const PORT: number | string = process.env.PORT || 8080;
 
+app.use(errorHandler);
+
 app.get("/", (_req: Request, res: Response) => {
   res.status(200).send("Welcome to Dinner for two!");
+});
+
+app.post("/login", (req: Request, res: Response) => {
+  
 });
 
 app.listen(PORT, () => {
